2dbc0206db05ce69625a4e947939a7fe7ea4a1ec
theDakshJaitly <thedakshjaitly@gmail.com>
2025-09-20 17:43:41 +0530

    updated hardcoded redirects

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3001/api',
 });
 
 // Add auth token to requests
